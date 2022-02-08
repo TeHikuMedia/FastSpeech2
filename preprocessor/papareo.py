2ccd8e1487dab7e40205f761d23fb79cbd263eb5
Miles Thompson <utunga@gmail.com>
2022-02-08 04:36:09 +0000

    Small tweak to processing method - doesnt really do anything

--- preprocessor/papareo.py
+++ preprocessor/papareo.py
@@ -3,15 +3,14 @@ import csv
 import librosa
 import numpy as np
 from scipy.io import wavfile
-from tqdm import tqdm
-
 from text import _clean_text
+from tqdm import tqdm
 
 def read_metadata_csv(metadata_csv_path):
     with open(metadata_csv_path, 'r', encoding="utf-8") as file:
         reader = csv.reader(file, quoting=csv.QUOTE_ALL)
         return list(reader)
-
+    
 def prepare_align(config):
     in_dir = config["path"]["corpus_path"]
     out_dir = config["path"]["raw_path"]
@@ -20,10 +19,9 @@ def prepare_align(config):
     cleaners = config["preprocessing"]["text"]["text_cleaners"]
     speaker = "peterl"
 
-    metadata_csv_path = os.path.join(in_dir, "metadata.csv")
-    metadata = read_metadata_csv(metadata_csv_path)
-    #metadata = metadata[0:100]
-    for row in tqdm(metadata):
+    metadata_csv_path = os.path.join(in_dir, "metadata.csv") 
+
+    for row in tqdm(read_metadata_csv(metadata_csv_path)):
         base_name = row[0]
         text = row[1]
         text = _clean_text(text, cleaners)
